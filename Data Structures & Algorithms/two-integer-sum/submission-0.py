29858b2a34b040439d66ac09735cd3a40060cf3f
NFSUPERCARS <110696574+nikF2458@users.noreply.github.com>
2026-08-08 12:08:57 +0300

    Delete submission-0.py

--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        l={}
-        for n in range(len(nums)):
-            diff=target-nums[n]
-            if diff in l:
-                return [l[diff],n]
-            l[nums[n]] = n
